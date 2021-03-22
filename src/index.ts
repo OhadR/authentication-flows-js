0f3cedd07fb3316d82d210bfb3474bee063aa507
OhadR <ohadr.developer@gmail.com>
2021-03-23 00:01:53 +0200

    export the web server

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './types/account-state';
 export * from './types/authentication-policy';
 
 export * from './interfaces/authentication-flows-processor';
+
+export * from './web/user-action-controller';
