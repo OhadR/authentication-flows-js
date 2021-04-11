33c8624d0bb01d09dfb036f64414c2a10e3d64fd
OhadR <ohadr.developer@gmail.com>
2021-04-11 16:17:10 +0300

    indent

--- src/interceptors/email.ts
+++ src/interceptors/email.ts
@@ -7,30 +7,29 @@ export async function sendEmail(recipient: string,
     url: string) {
     debug( 'sending email from: ' + AuthenticationFlowsConfig.instance.emailSender );
 
-    try {
-        const transporter = nodemailer.createTransport({
-            host: "mail.smtp2go.com",
-            port: 2525,
-            auth: {
-                user: AuthenticationFlowsConfig.instance.emailServerUser,
-                pass: AuthenticationFlowsConfig.instance.emailServerPass
-            }
-        });
 
-        const mailOptions = {
-            from: AuthenticationFlowsConfig.instance.emailSender,
-            to: recipient,
-            // from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            // to: "bar@example.com, baz@example.com", // list of receivers
-            subject,
-            text: url
-        };
+        try {
+            const transporter = nodemailer.createTransport({
+                host: "mail.smtp2go.com",
+                port: 2525,
+                auth: {
+                    user: AuthenticationFlowsConfig.instance.emailServerUser,
+                    pass: AuthenticationFlowsConfig.instance.emailServerPass
+                }
+            });
 
-        await transporter.sendMail(mailOptions);
-        debug( 'email was sent successfully.' );
-    }
-    catch (me) {
-        debug( me );
-        throw new AuthenticationFlowsError( me );
+            const mailOptions = {
+                from: AuthenticationFlowsConfig.instance.emailSender,
+                to: recipient,
+                // from: '"Fred Foo 👻" <foo@example.com>', // sender address
+                // to: "bar@example.com, baz@example.com", // list of receivers
+                subject,
+                text: url
+            };
+            await transporter.sendMail(mailOptions);
+            debug('email was sent successfully.');
+        } catch (me) {
+            debug(me);
+            throw new AuthenticationFlowsError(me);
     }
 }
